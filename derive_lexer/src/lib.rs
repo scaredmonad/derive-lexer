0a8ebc9d9f2deb123c1d3b9c9a1b6674608efe6c
scaredmonad <voidptr9@gmail.com>
2024-02-20 17:19:08 +0300

    chore: proc_macro receives input, no expansion

--- derive_lexer/src/lib.rs
+++ derive_lexer/src/lib.rs
@@ -5,3 +5,21 @@ pub trait Tokenize {
     where
         Self: Sized;
 }
+
+mod test {
+    use crate::derive_lexer_macro::Tokenize;
+    use crate::Tokenize;
+
+    #[derive(Tokenize, Debug)]
+    enum Token {
+        #[lex(pattern = "@")]
+        Decorator,
+        #[lex(pattern = "let")]
+        LetBinding,
+    }
+
+    #[test]
+    fn can_derive_tokens() {
+
+    }
+}
--- derive_lexer_macro/src/lib.rs
+++ derive_lexer_macro/src/lib.rs
@@ -1,3 +1,58 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::parse_macro_input;
+use syn::{parse_macro_input, Data, DataEnum, DeriveInput, Lit, Meta, NestedMeta, Variant};
+
+#[proc_macro_derive(Tokenize, attributes(lex))]
+pub fn tokenize_derive(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let enum_name = input.ident;
+
+    let variants = if let Data::Enum(DataEnum { variants, .. }) = input.data {
+        variants
+    } else {
+        panic!("Tokenize can only be derived for enums");
+    };
+
+    let match_arms = variants.into_iter().map(
+        |Variant {
+             ident: variant_name,
+             attrs,
+             ..
+         }| {
+            let pattern = attrs
+                .into_iter()
+                .find_map(|attr| {
+                    if let Meta::List(meta) = attr.parse_meta().expect("Error parsing meta") {
+                        if meta.path.is_ident("lex") {
+                            if let Some(NestedMeta::Meta(Meta::NameValue(meta_nv))) =
+                                meta.nested.first()
+                            {
+                                if let Lit::Str(lit) = &meta_nv.lit {
+                                    return Some(lit.value());
+                                }
+                            }
+                        }
+                    }
+                    None
+                })
+                .expect("Pattern not found");
+
+            quote! {
+                if input.starts_with(#pattern) {
+                    return Some((Self::#variant_name, &input[#pattern.len()..]));
+                }
+            }
+        },
+    );
+
+    let expanded = quote! {
+        impl Tokenize for #enum_name {
+            fn tokenize(input: &str) -> Option<(Self, &str)> {
+                #(#match_arms)*
+                None
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
+}
