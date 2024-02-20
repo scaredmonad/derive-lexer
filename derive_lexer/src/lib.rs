5978a1963ff9654120cccae769f2177540450c39
scaredmonad <voidptr9@gmail.com>
2024-02-20 17:41:07 +0300

    feat: derives tokens (failing)

--- derive_lexer/src/lib.rs
+++ derive_lexer/src/lib.rs
@@ -6,6 +6,7 @@ pub trait Tokenize {
         Self: Sized;
 }
 
+#[cfg(test)]
 mod test {
     use crate::derive_lexer_macro::Tokenize;
     use crate::Tokenize;
@@ -20,6 +21,15 @@ mod test {
 
     #[test]
     fn can_derive_tokens() {
+        let input = "@k let n";
+        let mut curr_input = input;
+        let mut tokens = Vec::new();
 
+        while let Some((token, rest)) = Token::tokenize(curr_input) {
+            tokens.push(token);
+            curr_input = rest;
+        }
+
+        dbg!(tokens);
     }
 }
